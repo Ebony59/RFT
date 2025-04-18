d4b7d5113f9692d6e924dbab52efd3e7968306f4
ebony <fairy3350@163.com>
2025-04-18 10:10:10 +0000

    update generate_data

--- generate_data.py
+++ generate_data.py
@@ -2,17 +2,12 @@ import torch
 import datasets
 import pandas as pd
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from more_itertools import chunked
 
 from tqdm import tqdm
 
-def generate_solution(
-    pipe,
-    question,
-    k=10,
-    verbose=False,
-) -> datasets.Dataset:
-    
-    prompt = """
+def generate_message(question):
+    system_prompt = """
     You are an AI assistant to solve maths problems. For each question, write a step-by-step solution and give the final answer in format: solution\n#### final_answer
     Only include the solution and answer, do not include any other descriptions.
     
@@ -28,11 +23,24 @@ def generate_solution(
     """
 
     messages = [
-      {"role": "system", "content": prompt},
+      {"role": "system", "content": system_prompt},
       {"role": "user", "content": "Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?"},
       {"role": "assistant", "content": 'Natalia sold 48/2 = <<48/2=24>>24 clips in May.\nNatalia sold 48+24 = <<48+24=72>>72 clips altogether in April and May.\n#### 72'},
       {"role": "user", "content": question}
     ]
+
+    return messages
+    
+
+def generate_solution(
+    pipe,
+    questions,
+    k=10,
+    verbose=False,
+) -> datasets.Dataset:
+    
+    prompts = [generate_message(q) for q in questions for i in range(k)]
+    temp_ds = datasets.Dataset.from_dict({"messages": prompts})
 
     generation_args = {
       "max_new_tokens": 500,
@@ -41,26 +49,13 @@ def generate_solution(
       "temperature": 0.7,
     }
 
-    outputs = pipe([messages] * k, **generation_args)
+    with torch.inference_mode():
+        outputs = pipe(temp_ds['messages'], **generation_args)
     responses = []
 
     for i, output in enumerate(outputs):
         text = output[0]['generated_text']
-        if verbose:
-            print(f"================{i}================")
-            print(text)
         responses.append(text)
-            
-    
-    # for i in range(k):
-    #     output = pipe(messages, **generation_args)
-    #     text = output[0]['generated_text']
-
-    #     responses.append(text)
-
-    #     if verbose:
-    #         print(f"================{i}================")
-    #         print(text)
 
     return responses
         
@@ -71,6 +66,12 @@ def generate_synthetic_data(model, tokenizer, ds, k=10, samples='all', verbose=F
     questions = ds['question']
     answers = ds['answer']
 
+    chunk_size = 128
+    question_chunks = list(chunked(questions, chunk_size))
+    answer_chunks = list(chunked(answers, chunk_size))]
+
+    print(f"Number of chunks: {len(question_chunks)}, chunk_size: {chunk_size}")
+
     if samples != 'all':
         questions = questions[:samples]
         answers = answers[:samples]
@@ -79,22 +80,25 @@ def generate_synthetic_data(model, tokenizer, ds, k=10, samples='all', verbose=F
         "text-generation",
         model=model,
         tokenizer=tokenizer,
+        batch_size=32
     )
 
-    for question, answer in tqdm(zip(questions, answers), total=len(questions)):
-        gt_answer = answer.split("####")[1].lstrip(' ').strip()
-        if verbose:
-            print('question:', question)
-            print('gt_answer:', answer)
-        responses = generate_solution(pipe, question, k, verbose)
+    for chunk_idx, (question_chunk, answer_chunk) in enumerate(tqdm(zip(question_chunks, answer_chunks), total=len(questions))):
+        print(f"Processing chunk {chunk_idx}")
+        gt_answers = [answer.split("####")[1].lstrip(' ').strip() for answer in answer_chunk]
+        
+        responses = generate_solution(pipe, question_chunk, k, verbose)
+
         for i, response in enumerate(responses):
+            question = question_chunk[i // k]
+            gt_answer = gt_answers[i // k]
             try:
                 response_answer = response.split("####")[1].lstrip(' ').strip()
                 if gt_answer == response_answer:
                     correct = True
                 else:
                     correct = False
-                dataset.append({'question': question, 'gt_answer': answer, 'llm_answer': response, 'correct': correct})
+                dataset.append({'question': question, 'gt_answer': gt_answer, 'llm_answer': response, 'correct': correct})
             except:
                 continue
 
@@ -114,6 +118,7 @@ if __name__ == "__main__":
         device_map="cuda",
         torch_dtype="auto",
         trust_remote_code=True,
+        attn_implementation="flash_attention_2"
     )
     tokenizer = AutoTokenizer.from_pretrained(MODEL)
     
